3e3d1ef74736bd69d3af174b9a190f548fd03d07
Ebenezer <bamisayebenezer@gmail.com>
2025-03-29 06:39:49 +0000

    my second commit

--- variables.tf
+++ variables.tf
@@ -6,15 +6,6 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "instance_ami" {
-  default = "ami-0ea0a27a4a8e7b2b0"
-}
-
-
-variable "vpc_id" {
-  default = ""
-}
-
 
 variable "key_name" {
   default = "cba_terrakey"
